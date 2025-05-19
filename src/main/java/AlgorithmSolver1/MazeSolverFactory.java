e960dfbab21065eae24cd656c1e84db4d5528bec
Amine_kh <amine.khad04@gmail.com>
2025-05-19 18:01:43 +0200

    Update MazeSolverFactory.java

--- src/main/java/AlgorithmSolver1/MazeSolverFactory.java
+++ src/main/java/AlgorithmSolver1/MazeSolverFactory.java
@@ -1,1 +1,19 @@
+package src.main.java.AlgorithmSolver1;
 
+public class MazeSolverFactory {
+    
+    public static MazeSolver createSolver(String algorithm) {
+        switch (algorithm) {
+            case "BFS":
+                return new BFSSolver();
+            case "Algorithme A*":
+                // À implémenter ultérieurement
+                return new BFSSolver(); // Pour le moment, on utilise BFS
+            case "Algorithme de Dijkstra":
+                // À implémenter ultérieurement
+                return new BFSSolver(); // Pour le moment, on utilise BFS
+            default:
+                return new BFSSolver();
+        }
+    }
+}
