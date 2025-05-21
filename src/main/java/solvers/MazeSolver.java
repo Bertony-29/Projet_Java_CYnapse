7693cbf2905a6719fd1c08ee4b6d29c7ff312158
Amine_kh <amine.khad04@gmail.com>
2025-05-21 13:42:11 +0200

    Update MazeSolver.java

--- src/main/java/solvers/MazeSolver.java
+++ src/main/java/solvers/MazeSolver.java
@@ -1,1 +1,2 @@
-#l’interface commune à tous tes algorithmes de résolution de labyrinthe.
+//l’interface commune à tous tes algorithmes de résolution de labyrinthe.
+//garanti une méthode solve() que tous les solvers doivent implémenter
